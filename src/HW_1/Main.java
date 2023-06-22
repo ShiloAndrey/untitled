659eeb41f34cdada433b0f96638bbf195478b57e
Андрей <andrey.shilo@bk.ru>
2023-06-23 00:16:05 +0500

    HW_1

---
+++ src/HW_1/Main.java
@@ -0,0 +1,48 @@
+package HW_1;
+
+public class Main {
+public static int y = 256;
+public static Integer Y = 258;
+    public static void main(String[] args) {
+        Main2 peremennaya = new Main2();
+        byte numb = Main2.getNumb();
+        System.out.println(numb);
+        short numb2 = Main2.getNumb2();
+        System.out.println(numb2);
+        int numb3 = Main2.getNumb3();
+        System.out.println(numb3);
+        long numb4 = Main2.getNumb4();
+        System.out.println(numb4);
+        float f = Main2.getF();
+        System.out.println(f);
+        double d = Main2.getD();
+        System.out.println(d);
+        char c = Main2.getC();
+        System.out.println(c);
+        boolean b = Main2.isB();
+        System.out.println(b);
+        Byte Numb = Main2.getNumber1();
+        System.out.println(Numb);
+        Short Numb2 = Main2.getNumber2();
+        System.out.println(Numb2);
+        Integer Numb3 = Main2.getNumber3();
+        System.out.println(Numb3);
+        Long Numb4 = Main2.getNumber4();
+        System.out.println(Numb4);
+        Float F1 = Main2.getF1();
+        System.out.println(F1);
+        Double D1 = Main2.getD1();
+        System.out.println(D1);
+        Character C1 = Main2.getC1();
+        System.out.println(C1);
+        Boolean B1 = Main2.getB1();
+        System.out.println(B1);
+        int x = 113;
+        Byte X = 114;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(Y);
+        System.out.println(X);
+    }
+}
+
---
+++ src/HW_1/Main2.java
@@ -0,0 +1,147 @@
+package HW_1;
+
+public class Main2 {
+    private static byte numb = 127;
+    private static short numb2 = 32767;
+    private static int numb3 = 2147000000;
+    private static long numb4 = 999999999999L;
+    private static float f = 3.14F;
+    private static double d = 3.15;
+    private static char c = 'f';
+    private static boolean b = true;
+    private static Byte Number1 = 111;
+    private static Short Number2 = 12563;
+    private static Integer Number3 = 2147000003;
+    private static Long Number4 = 88888888888888L;
+    private static Float F1 = 6.28F;
+    private static Double D1 = 6.3;
+    private static Character C1 = 'D';
+    private static Boolean B1 = false;
+
+    public static byte getNumb() {
+        return numb;
+    }
+
+    public static Byte getNumber1() {
+        return Number1;
+    }
+
+    public static Float getF1() {
+        return F1;
+    }
+
+    public void setNumber1(Byte numb) {
+        Number1 = numb;
+    }
+
+    public void setNumb(byte numb) {
+        this.numb = numb;
+    }
+
+    public static short getNumb2() {
+        return numb2;
+    }
+
+    public static void setNumber2(Short numb2) {
+        Number2 = numb2;
+    }
+    public static Short getNumber2() {
+        return Number2;
+    }
+
+    public void setNumb2(short numb2) {
+        this.numb2 = numb2;
+    }
+
+    public static int getNumb3() {
+        return numb3;
+    }
+
+    public static void setNumber3(Integer numb3) {
+        Number3 = numb3;
+    }
+
+    public static Integer getNumber3() {
+        return Number3;
+    }
+
+    public void setNumb3(int numb3) {
+        this.numb3 = numb3;
+    }
+
+    public static long getNumb4() {
+        return numb4;
+    }
+
+    public static void setNumber4(Long numb4) {
+        Number4 = numb4;
+    }
+    public static Long getNumber4() {
+        return Number4;
+    }
+
+    public void setNumb4(long numb4) {
+        this.numb4 = numb4;
+    }
+
+    public static float getF() {
+        return f;
+    }
+
+    public void setF1(Float f) {
+        F1 = f;
+    }
+
+
+    public void setF(float f) {
+        this.f = f;
+    }
+
+    public static double getD() {
+        return d;
+    }
+
+    public void setD1(Double d) {
+        D1 = d;
+    }
+
+    public static Double getD1() {
+        return D1;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public static char getC() {
+        return c;
+    }
+
+    public void setC1(Character c) {
+        C1 = c;
+    }
+
+    public static Character getC1() {
+        return C1;
+    }
+
+    public static Boolean getB1() {
+        return B1;
+    }
+
+    public void setB(Boolean b) {
+        B1 = b;
+    }
+
+    public void setC(char c) {
+        this.c = c;
+    }
+
+    public static boolean isB() {
+        return b;
+    }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+}
